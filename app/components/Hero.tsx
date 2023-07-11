c4032dc4c14718a28373b6af444a61a8ccd67e87
Radicitus <novivolatus@gmail.com>
2023-07-11 01:00:14 -0700

    Small hack to make Hero h = 100vh - 80px for nav bar

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -9,7 +9,7 @@ export default function Hero({
   buttons,
 }: heroParamsType) {
   return (
-    <div className="hero h-screen lg:h-128">
+    <div className="hero h-[calc(100vh-80px)] lg:h-128">
       <div className="hero-content flex-col lg:flex-row-reverse text-center">
         <div className="avatar">
           <div className="w-72 lg:w-80 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
