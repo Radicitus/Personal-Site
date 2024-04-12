3a16e6d09a27e51561f3634c55beb1cc84f72b3c
Radicitus <novivolatus@gmail.com>
2024-04-11 23:40:53 -0400

    Update to v0.10.0 from v0.3.2 of apollo/experimental-nextjs-app-support

--- graphql/clients/serverSideClient.ts
+++ graphql/clients/serverSideClient.ts
@@ -1,14 +1,11 @@
-import { HttpLink } from "@apollo/client";
-import {
-  NextSSRInMemoryCache,
-  NextSSRApolloClient,
-} from "@apollo/experimental-nextjs-app-support/ssr";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 
 export const { getClient } = registerApolloClient(() => {
-  return new NextSSRApolloClient({
-    cache: new NextSSRInMemoryCache(),
+  return new ApolloClient({
+    cache: new InMemoryCache(),
     link: new HttpLink({
+      // this needs to be an absolute url, as relative urls cannot be used in SSR
       uri: process.env.CMS_URL,
       // you can disable result caching here if you want to
       // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
