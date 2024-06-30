26a2e14c2fa6350586ddd428b12eaa1d2d6a229e
Radicitus <novivolatus@gmail.com>
2024-06-29 21:36:37 -0400

    Remove unused files and packages

--- components/Lottie/ProjectsAnimation.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { Player } from "@lottiefiles/react-lottie-player";
-import projectsAnimation from "@/public/projectsRocket.json";
-
-export default function ProjectsAnimation() {
-  return <Player autoplay loop src={projectsAnimation} />;
-}
