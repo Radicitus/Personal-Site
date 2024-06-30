d38ee3556530163c8eab2de20990815b4491cb9e
Radicitus <novivolatus@gmail.com>
2024-06-29 23:21:16 -0400

    Add static nav links

--- components/Marginals/Nav/NavContainer.tsx
+++ components/Marginals/Nav/NavContainer.tsx
@@ -1,8 +1,5 @@
-import { getClient } from "@/graphql/clients/serverSideClient";
-import { GET_NAV_PAGES } from "@/graphql/queries/page";
 // TYPES
 import { LinkType } from "@/types/linkType";
-import { PageSearchResultType } from "@/types/strapi/pageSearchResultType";
 // HELPERS
 import { isMobileDevice } from "@/utils/isMobileDevice";
 // COMPONENTS
@@ -12,27 +9,15 @@ import NavDrawer from "@/components/Marginals/Nav/NavDrawer";
 export const revalidate = 60;
 
 export default async function NavContainer() {
-  // Get page nav data from Strapi
-  const client = getClient();
-  const res: PageSearchResultType = await client.query({
-    query: GET_NAV_PAGES,
-  });
-
-  let pages = res.data.pages.data;
-  // Inject page title into link objects
-  let pageLinks: LinkType[] = pages.map((page) => {
-    let pageLink = page.attributes.link.data.attributes;
-    let link: LinkType = {
-      title: page.attributes.title,
-      path: pageLink.path,
-      target: pageLink.target,
-    };
-    return link;
-  });
+  const navLinks: LinkType[] = [
+    { title: "Experience", path: "/experience", target: "_self" },
+    { title: "Projects", path: "/projects", target: "_self" },
+    { title: "About", path: "/about", target: "_self" },
+  ];
 
   return isMobileDevice() ? (
-    <NavDrawer links={pageLinks} />
+    <NavDrawer links={navLinks} />
   ) : (
-    <NavBar links={pageLinks} />
+    <NavBar links={navLinks} />
   );
 }
