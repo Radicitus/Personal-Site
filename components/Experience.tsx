ee406b816c672f5444b2461534da92fd6932443f
Radicitus <novivolatus@gmail.com>
2024-07-01 22:17:33 -0400

    Remove experience and projects components

--- components/Experience.tsx
+++
@@ -1,7 +0,0 @@
-export default function Experience() {
-  return (
-    <div className="flex h-screen w-full flex-col pt-20" id="experience">
-      <p className="text-center text-4xl">Experience</p>
-    </div>
-  );
-}
--- components/Projects.tsx
+++
@@ -1,7 +0,0 @@
-export default function Projects() {
-  return (
-    <div className="flex h-screen w-full flex-col pt-20" id="projects">
-      <p className="text-center text-4xl">Projects</p>
-    </div>
-  );
-}
