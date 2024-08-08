872e63cab0e97bbb77256c9a23c448c2bebc13ce
Cam <novivolatus@gmail.com>
2024-08-08 20:41:11 +0000

    Add test text

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,17 @@ export default function Home() {
         </main>
       ) : (
         <main id="home" className="flex h-screen w-full justify-center">
-          <DynamicBadge />
+          <div className="relative w-full">
+            <DynamicBadge />
+
+            <div className="absolute top-20 text-4xl">
+              <span className="z-40">Test1</span>
+              <span className="z-40">Test2</span>
+              <span className="z-40">Test3</span>
+              <span className="z-40">Test4</span>
+              <span className="z-40">Test5</span>
+            </div>
+          </div>
         </main>
       )}
     </>
