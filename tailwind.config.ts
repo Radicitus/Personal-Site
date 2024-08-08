fedade6ad5ca9037c5768352845ff26be52a56f3
Cam <novivolatus@gmail.com>
2024-08-08 19:33:22 +0000

    Add tailwind extensions for background scrolling banner

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -87,10 +87,15 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        loop: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        loop: "loop 25s linear infinite",
       },
       fontFamily: {
         mohave: ["var(--font-mohave)"],
