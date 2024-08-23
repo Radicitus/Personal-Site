c7886f9865826075d9c67b3d6727a961cae827ff
Cameron Sherry <novivolatus@gmail.com>
2024-08-23 20:22:09 +0000

    Exp checkpoint

--- components/ExperienceScroller.jsx
+++ components/ExperienceScroller.jsx
@@ -1,9 +1,9 @@
 
 export default function ExperienceScroller() {
     return (
-        <div className="box-border m-auto h-64 w-32 p-4 
-                border-4 bg-green-500 m-4">
-        A Computer Science Portal
-    </div>
+        <div className="flex flex-row text-center text-black">
+            <div className="w-1/2 bg-white h-64">Test</div>
+            <div className="w-1/2 bg-white">Test</div>
+        </div>
     )
 }
