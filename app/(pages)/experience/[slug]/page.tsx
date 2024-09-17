0e51729466cea8668acefe9f742b1c4ec92f2f49
Radicitus <novivolatus@gmail.com>
2024-09-17 10:58:15 -0400

    Add comments

--- app/(pages)/experience/[slug]/page.tsx
+++ app/(pages)/experience/[slug]/page.tsx
@@ -36,16 +36,20 @@ export default async function ExperiencePage({
   return (
     <div className="text-white">
       <div className="mx-10 mt-16 md:mx-24 lg:mx-40 xl:mx-56 2xl:mx-80">
+        {/* Experience Header */}
         <h5
           className="animate-text bg-gradient-to-r from-amber-500 to-pink-500 bg-clip-text uppercase text-transparent
           transition-all duration-300 ease-in-out"
         >
           Experience
         </h5>
+
+        {/* Company Name*/}
         <h1 className="mt-5 font-mohave text-6xl font-bold">
           {experience.attributes.company}
         </h1>
 
+        {/* Title and Dates */}
         <div className="mb-5 flex flex-col sm:flex-row">
           <h4 className="text-2xl font-light">{experience.attributes.title}</h4>
           <div className="hidden self-center px-2.5 pt-1 font-light opacity-70 sm:block">
@@ -72,12 +76,15 @@ export default async function ExperiencePage({
           </h6>
         </div>
 
+        {/* Summary */}
         <h4 className="italic">{experience.attributes.summary}</h4>
 
+        {/* Main Content */}
         <div className="my-12 text-justify">
           <BlockRendererClient content={experience.attributes.content} />
         </div>
 
+        {/* Key Contributions */}
         <div className="mx-5 mb-16 md:mx-14 lg:mx-24 xl:mx-36 2xl:mx-52">
           <div className="text-center font-mohave text-2xl uppercase">
             Key Contributions
@@ -89,6 +96,7 @@ export default async function ExperiencePage({
           </div>
         </div>
 
+        {/* Technologies */}
         <div className="mx-28 mb-16">
           <div className="text-center font-mohave text-2xl uppercase">
             Technologies
