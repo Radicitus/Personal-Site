5c892e0c1f35ae0b8287cc939d1c9c95b96a6d2f
Radicitus <novivolatus@gmail.com>
2024-09-20 21:30:16 -0400

    Add icon component and update footer

---
+++ components/Icon.tsx
@@ -0,0 +1,36 @@
+import { Github, Linkedin, Mail } from "lucide-react";
+import Link from "next/link";
+import { IconType } from "@/types/IconType";
+
+export default function Icon({ icon }: { icon: IconType }) {
+  if (icon.attributes.type === "email") {
+    return (
+      <Link
+        href={icon.attributes.link.data.attributes.path}
+        target={"_" + icon.attributes.link.data.attributes.target}
+      >
+        <Mail />
+      </Link>
+    );
+  }
+  if (icon.attributes.type === "github") {
+    return (
+      <Link
+        href={icon.attributes.link.data.attributes.path}
+        target={"_" + icon.attributes.link.data.attributes.target}
+      >
+        <Github />
+      </Link>
+    );
+  }
+  if (icon.attributes.type === "linkedin") {
+    return (
+      <Link
+        href={icon.attributes.link.data.attributes.path}
+        target={"_" + icon.attributes.link.data.attributes.target}
+      >
+        <Linkedin />
+      </Link>
+    );
+  }
+}
