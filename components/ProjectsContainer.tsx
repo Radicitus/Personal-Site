6198059d368859e4cc131118de2371f4dcca53e1
Radicitus <novivolatus@gmail.com>
2024-09-20 20:19:47 -0400

    Add additional styling and remove date

--- components/ProjectsContainer.tsx
+++ components/ProjectsContainer.tsx
@@ -12,11 +12,11 @@ export default async function ProjectsContainer() {
 
   if (projects.length) {
     return (
-      <div className="bg-black pb-24">
+      <div className="bg-black pb-24" id="projects">
         {/* Projects Header */}
         <h2
-          className="h-26 select-none bg-black pb-24 pt-8 text-center font-mohave text-4xl font-semibold
-        uppercase tracking-[.35em] sm:text-8xl"
+          className="h-26 select-none bg-black pb-24 text-center font-mohave text-4xl font-semibold uppercase
+        tracking-[.35em] sm:mt-20 sm:text-8xl"
         >
           Projects
         </h2>
@@ -29,40 +29,59 @@ export default async function ProjectsContainer() {
               .map((project) => (
                 <Link
                   href={"/project/" + project.attributes.slug}
-                  className="mx-6 mb-4 flex min-h-32 max-w-screen-lg flex-row justify-between border-b-[.5px] border-white
-                border-opacity-30 px-6 pb-4 transition-all duration-300
-                first:border-t-[.5px] first:pt-4 hover:!scale-105 hover:!opacity-100 group-hover:opacity-50 sm:mx-14"
+                  className="group/item mx-6 flex min-h-32 flex-row justify-between border-t-[.5px] border-white border-opacity-30
+                px-6 py-4 transition-all duration-300 ease-in-out last:border-b-[.5px]
+                hover:scale-[1.02] hover:!opacity-100 group-hover:opacity-50 sm:mx-14"
                   key={project.attributes.slug}
                 >
-                  <div>
-                    <div className="text-xl font-bold">
+                  <div className="lg:mr-24">
+                    {/* Title */}
+                    <div
+                      className="bg-gradient-to-r from-amber-500 to-pink-500 bg-clip-text font-inter text-xl font-bold
+                    transition-all duration-300 ease-in-out group-hover/item:animate-text group-hover/item:text-transparent lg:text-2xl"
+                    >
                       {project.attributes.title}
                     </div>
-                    <div className="mr-8 pt-2 text-sm">
+
+                    {/* Description */}
+                    <div className="mr-8 pt-1 text-sm lg:pt-2 lg:text-base">
                       {project.attributes.description}
                     </div>
-                    <h6 className="pt-2 text-sm font-light">
-                      {new Date(project.attributes.start).toLocaleDateString(
-                        "default",
-                        {
-                          month: "short",
-                          year: "numeric",
-                        }
-                      )}{" "}
-                      -{" "}
-                      {project.attributes.end
-                        ? new Date(project.attributes.end).toLocaleDateString(
-                            "default",
-                            {
-                              month: "short",
-                              year: "numeric",
+
+                    {/* Technologies */}
+                    {project.attributes.technologies.data.length ? (
+                      <div className="flex flex-row pt-3 lg:pt-5">
+                        {/*<div className="text-sm uppercase">Technologies</div>*/}
+                        <div className="flex flex-row flex-wrap gap-2 text-sm opacity-65 lg:text-base">
+                          {project.attributes.technologies.data.map(
+                            (tech, index) => {
+                              if (index == 0) {
+                                return (
+                                  <div key={tech.attributes.title}>
+                                    {tech.attributes.title}
+                                  </div>
+                                );
+                              } else {
+                                return (
+                                  <div
+                                    className="flex flex-row gap-2"
+                                    key={tech.attributes.title}
+                                  >
+                                    <div>•</div>
+                                    <div>{tech.attributes.title}</div>
+                                  </div>
+                                );
+                              }
                             }
-                          )
-                        : "Present"}
-                    </h6>
+                          )}
+                        </div>
+                      </div>
+                    ) : null}
                   </div>
+
+                  {/* Cover Image */}
                   <Image
-                    className="mr-3 h-28 w-28 rounded object-cover object-center opacity-80"
+                    className="h-28 w-28 place-self-center rounded object-cover object-center opacity-80 lg:h-32 lg:w-32"
                     src={project.attributes.cover.data.attributes.url}
                     alt={
                       project.attributes.cover.data.attributes.alternativeText
